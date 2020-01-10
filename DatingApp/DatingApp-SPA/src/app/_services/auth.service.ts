3652bedbad9b37d378b74bd6b9a947ce3c95a2d0
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-01-10 11:25:54 +0200

    4.43 Injecting the Angular services in the Components
    -using the auth service instead of logging out to the console.
    -injected the auth service into the constructor.
    -used the auth service instead of logging out to the console.

--- DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -13,7 +13,7 @@ constructor(private http: HttpClient) { }
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(
-      map((response: any) =>{
+      map((response: any) => {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
