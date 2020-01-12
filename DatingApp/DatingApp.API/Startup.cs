ce90c6ba6b37f1eb1983000d1e33fddd3e2924e6
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-01-12 16:51:06 +0200

    5.52 Setting up the Global exception handler in the API
    -configured a global exception handler in ASP.NET Core instead of surrounding my code with try-catch blocks.
    -->in the Startup class, in the else block(if we are I'm not in development mode):
    
    -I made use of the global exception handler.
    -when I go into the exception handler and I use the Run method, I'm executing the request in a different pipeline and I'm returning the response from this exception (I'm retruning a message that does not have CORS headers applied to it.)
    
    -extended the Response so I can added my custom app error to this response.
    -->used an extension method:
    -created a new Folder "Helpers" and a new C# class "Extensions".
    -this class is going to be a general purpose extesions class that I'll use for other extensions method needed.
    -I don't want to create an new instance of the class in order to use the methods inside. Because of this I made the class static.
    -I added additional headers into my response.
    
    -

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +17,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -58,6 +62,22 @@ namespace DatingApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                           await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             //app.UseHttpsRedirection();
