f0ead14452a6193888230928d7ed9e3fdbb42916
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-01-16 10:36:21 +0200

    9.89 Using Auth0 JwtModule to send up jwt tokens automaticaly
    -used @auth0/angular-jwt to decode and check if the token is expired or not.
    -I get the token inside the app module -->tokenGetter function.
    -I imported the jwt module so I can configure it to send up a token for anything I'm sending to what if cast as a whitelisted domain. Any blacklisted routes --> not going to send the token.( so I'm not sending up the token when I'm querying the auth API.
    
    -in the tokenGetter function, I'm returning the token from local storage.
    -back in the user service I removed the http options from my requests.
    -now the authorization bearer token is being send up when I login(just as it was when I added the headers manually), but now I don't need to add headers for any other requests that I need to send an auth token to. Any req that goes to my API is going to have this automatically added.(unless I logout and login again-->doesn't have the auth header, because it is part of the blacklisted routes)

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
-
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,6 +19,9 @@ import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-list/member-card/member-card.component';
 
 
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 
 @NgModule({
@@ -37,7 +40,14 @@ import { MemberCardComponent } from './members/member-list/member-card/member-ca
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
