fc381eeb219b3328354425507a175f2d4dcc6b2d
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-01-16 11:42:21 +0200

    9.90 Creating the Member Detailed View component class
    -redirected the user to a detailed view of the user when they click on the user-icon button ( the first button that appears on the picture.)
    -I already that the method inside user.service.ts to get an individual user so I need to get hold of the id of the selected user.
    -created a new component-->member-detail.
    -added it inside the app module.
    -inside the member-detailed.ts --> I need to go and get my detailed user from the API.
    -added a property for user (of type user).
    -created the loadUser method in which I pass the id of the user that I'm selecting -->when a user clicks on another user,I will get the 'id' part of the route (members/4) and pass this as the id parameter for my method.
    -to get access of this parameter,I brought in a part of the router service(when the route is activated, I'll have access to that particular parameter).
    -because the url is a string and I want the id to be a number I added the + to convert it.
    -handled the error condition.
    -called the loadUser method inside ngOnInit for this component.
    -configured a new route so users can get to this particular component.
    -inside the member-card.component.html added a router link to the button.
    -tested to see if the functionality is working.
    -getting an error if my components are loading before the data has been received and after the data has been received, it is put inside the html.
    -but the component's imediate reaction is to throw an error.
    -to fix this I added the save navigation operator '?'
    
    -

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-list/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
 
 
 export function tokenGetter() {
@@ -33,7 +34,8 @@ export function tokenGetter() {
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardComponent
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
--- DatingApp/DatingApp-SPA/src/app/routes.ts
+++ DatingApp/DatingApp-SPA/src/app/routes.ts
@@ -4,6 +4,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -11,7 +12,9 @@ export const appRoutes: Routes = [
       path: '',
       runGuardsAndResolvers: 'always',
       canActivate: [AuthGuard],
-      children: [{ path: 'members', component: MemberListComponent},
+      children: [
+      { path: 'members', component: MemberListComponent},
+      { path: 'members/:id', component: MemberDetailComponent},
       { path: 'messages', component: MessagesComponent},
       { path: 'lists', component: ListsComponent},
     ]
