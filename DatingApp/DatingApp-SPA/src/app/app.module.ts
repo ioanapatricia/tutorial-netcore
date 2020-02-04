16e0d5d7ad338488f3919c0cf46cde6a0492f9e0
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-04 12:23:10 +0200

    16.166 Working with the message component in the SPA
    -in the models folder I generated a new interface-->message --> contains the properties that are returned in the MessageToReturnDto(from the API).
    -created a method inside the user service to retrieve the messages:
    -specified what I'm returning-->message array.
    -the id in the parameters is the user id.
    -added a const for the paginated result.
    -created the parameters.
    -appended the parameters.
    -added the paging information to the params.
    -returned the url.
    -observed the full responde so I can have access to the pagination header that I'm getting from the API.
    -to use the map operator I added the pipe.
    -mapped the response.
    -line 90 --> contains the list of messages.
    -line 95 --> returns an observable of type paginated result of type message.
    -created a resolver for the messages to get the data in my route and pass the data inside the route so I can have access to what is inside there before the component load.
    -added a prop for the message container, set it to unread to match what is coming back from the API by default.
    -in the get messages method on line 20 I passed the user id, the paging info and the message container.
    -added the resolver to the app modules in the providers.
    -added the resolver to the routes.ts.
    -in the messages component:
    -added props for messages,pagination and message container.
    -on lines 23-25 I got the data.
    -added a method to load the messages.
    -got the messages from the user service and I passed the parameters.
    -subscribed and in this case I'm getting back a paginated result of type message array.
    -added the error condition.
    -added the page changed event.

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/member-list/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 
 
@@ -89,6 +90,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MemberEditResolver,
       PreventUnsavedChanges,
       ListsResolver,
+      MessagesResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
