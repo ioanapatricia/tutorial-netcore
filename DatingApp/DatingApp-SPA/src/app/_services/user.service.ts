d367e0e7cafb2bd3447a42c27353f9916dda677d
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-05 10:34:07 +0200

    16.171 Adding the send message functionality
    -in the user service :
    -added a method for sending a message that takes an id which is the logged in user.
    -in the messages component I added a new property of type any and I set it to an empty object.
    -in the html I used a angular form.
    -check that the message form is valid.
    -made the button disabled if there is no message.
    -back in the component I created the send message method.
    -passed the recipient id in the body of what I'm sending back to the server.
    -set the recipient id to the user's id of the detailed profile that I'm on.
    -because I want the message to be added at the start of the messages array I used the unshift method instead of push.
    -after the message is added I reseted the form on line 38.

--- DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
@@ -100,4 +100,8 @@ export class UserService {
   getMessageThread(id: number, recepientId: number) {
     return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recepientId);
   }
+
+  sendMessage(id: number, message: Message) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
+  }
 }
