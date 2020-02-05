d198fdfaf00a3057c5ee0147732cd6bdf22678d3
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-05 10:57:29 +0200

    16.172 Resolving the issue with the photos
    -watch the video

--- DatingApp/DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp/DatingApp.API/Controllers/MessagesController.cs
@@ -75,7 +75,9 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
             
             messageForCreationDto.SenderId = userId;
@@ -89,11 +91,13 @@ namespace DatingApp.API.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
             if (await _repo.SaveAll())
-                //var messageToReturn = _mapper.Map<MessageToReturn>(message);
-                return CreatedAtRoute("GetMessage", new {userId, id = message.Id}, messageToReturn); 
+            {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
+                return CreatedAtRoute("GetMessage", new {userId, id = message.Id}, messageToReturn);
+            }
+                
+                 
             
             throw new Exception("Creating the message failed on save");
         }
