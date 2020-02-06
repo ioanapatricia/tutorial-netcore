f430cbc267fdc34f937a4bf97108690a8125192a
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-06 13:11:11 +0200

    20.200 Configuring the Startup.cs class for Identity
    -added Identity configuration.
    -used the IdentityBuilder class.
    -used AddIdentityCore service--> adds and configures the identity system for a specified User type --> passed to it the user class --> I selected this one to have control over my Identity config.
    -passed some options to be able to use weak passwords(it's not a production/real app worthy config) -->I added the config to turn off the pass requirements to simply continue to use the passwords that I've been using until now for testing.
    -created a new Identity builder to add all the services that I need for Identity.
    -as arguments I passed user type, type of role and services.
    -the AddEntityFrameworkStores creates a user store so that I can add/create users. Also it creates a bunch of tables that are needed in the database to support the Identity system.
    -I moved the services.AddAuthentication up to keep the identity and authentication info in the same area.
    -added a authorization policy to require authentication by default.
    -I will allow anonymous only in a few specific areas of the app --> login and register methods.
    -provided some options into AddControllers to add the authorization policy.
    -added the allow anonymous in the auth controller where the login and register methods are.
    -i removed the [Authorize] attribute from the other controllers.

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -20,6 +20,10 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace DatingApp.API
 {
@@ -35,21 +39,20 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<DataContext>(options =>
-                options.UseSqlServer
-                (
-                    Configuration.GetConnectionString("datingAppDb")
-                ));
-            services.AddControllers().AddNewtonsoftJson(opt =>
+            IdentityBuilder builder = services.AddIdentityCore<User>(opt => 
             {
-                opt.SerializerSettings.ReferenceLoopHandling = 
-                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequiredLength = 4;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireUppercase = false;
             });
-            services.AddCors();
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-            services.AddAutoMapper(typeof(DatingRepository).Assembly);
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddScoped<IDatingRepository, DatingRepository>();
+
+            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+            builder.AddEntityFrameworkStores<DataContext>();
+            builder.AddRoleValidator<RoleValidator<Role>>();
+            builder.AddRoleManager<RoleManager<Role>>();
+            builder.AddSignInManager<SignInManager<User>>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>{
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -59,9 +62,32 @@ namespace DatingApp.API
                             .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
                         ValidateIssuer = false,
                         ValidateAudience = false
-
                     };
                 });
+
+            services.AddDbContextPool<DataContext>(options =>
+                options.UseSqlServer
+                (
+                    Configuration.GetConnectionString("datingAppDb")
+                ));
+            services.AddControllers(options => 
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                
+                options.Filters.Add(new AuthorizeFilter(policy));
+            })
+            .AddNewtonsoftJson(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = 
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+            services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddScoped<LogUserActivity>();
         }
 
