79d1be0c143aa150bde30929e8734a8a3c6efa0b
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-07 09:55:11 +0200

    20.202 Updating the Seed Users method
    -used the user manager in the seed class to create the seed data.
    -I have a static method to seed the users. I checked to see if there are any users in the db, then I read the users from my json data.
    -instead of passing the datacontext indirectly as an argument inside the seed users method, I used the user manager of type user.
    -used the user manager directly to see if there are any users in the db.
    -usermanager automatically saves the changes when I create a user with this method.
    -added the wait because the createasync is a async method and I'm not inside the context of an async method (method on line 11 is not async).
    -adjusted the program class.
    -got another service for the user manager.
    -passed the user manager to the seed users method.

--- DatingApp/DatingApp.API/Data/Seed.cs
+++ DatingApp/DatingApp.API/Data/Seed.cs
@@ -1,30 +1,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
 
 namespace DatingApp.API.Data
 {
     public class Seed
     {
-        public static void SeedUsers(DataContext context)
+        public static void SeedUsers(UserManager<User> userManager)
         {
-            if (!context.Users.Any())
+            if (!userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
                 foreach (var user in users)
                 {
-                    byte[] passwordhash, passwordSalt;
-                    CreatePasswordHash("password", out passwordhash, out passwordSalt);
-
-                    // user.PasswordHash = passwordhash;
-                    // user.PasswordSalt = passwordSalt;
-                    user.UserName = user.UserName.ToLower();
-                    context.Users.Add(user);
+                    userManager.CreateAsync(user, "password").Wait();
                 }
-
-                context.SaveChanges();
             }
         }
 
