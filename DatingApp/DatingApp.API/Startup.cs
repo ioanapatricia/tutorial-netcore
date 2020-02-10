f0b5f2461a5e8d3294287eb068f07c495f6fa76b
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-10 10:51:58 +0200

    20.207 Policy based authorization
    in the startup class I added another service.
    -configured the policies.
    -created a new controller --> AdminController.
    -derived from controller base.
    -gave this some attributes --> route and api controller.
    -created 2 actions inside that for now just return Ok.
    -made them http get methods.
    -added the authorize policies.

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -65,6 +65,14 @@ namespace DatingApp.API
                     };
                 });
 
+            services.AddAuthorization(options => 
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin",
+                "Moderator"));
+                options.AddPolicy("VipOnly", policy => policy.RequireRole("VIP"));
+            });
+
             services.AddDbContextPool<DataContext>(options =>
                 options.UseSqlServer
                 (
