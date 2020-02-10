fe7bceff8c7c5360d0bb42572efffa72a7ec3bb0
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-10 09:49:14 +0200

    20.205 Updating the seed method with Roles
    -passed the role manager as an argument to the seed method.
    -created some roles that I can put my users into.
    -used the object initializer to do this.
    -populated the roles in the database(lines 28-31).
    -took each var user and put them into the "Member" role (when I seed the data).
    -created an admin user.
    -gave the admin user a weak password(training app).
    -checked to see if the result succeded.
    -got the admin user.
    -gave it the role of admin and moderator.(passed an array of the roles that I want to add to the this particular user).
    -in the program class I got the role manager service.
    -updated my seed users method.

--- DatingApp/DatingApp.API/Data/Seed.cs
+++ DatingApp/DatingApp.API/Data/Seed.cs
@@ -8,15 +8,46 @@ namespace DatingApp.API.Data
 {
     public class Seed
     {
-        public static void SeedUsers(UserManager<User> userManager)
+        public static void SeedUsers(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             if (!userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                //create roles
+
+                var roles = new List<Role>
+                {
+                    new Role{Name = "Member"},
+                    new Role{Name = "Admin"},
+                    new Role{Name = "Moderator"},
+                    new Role{Name = "VIP"}
+                };
+
+                foreach (var role in roles)
+                {
+                    roleManager.CreateAsync(role).Wait();
+                }
+
                 foreach (var user in users)
                 {
                     userManager.CreateAsync(user, "password").Wait();
+                    userManager.AddToRoleAsync(user, "Member").Wait();
+                }
+
+                //create admin user
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                var result = userManager.CreateAsync(adminUser, "password").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = userManager.FindByNameAsync("Admin").Result;
+                    var addResult = userManager.AddToRolesAsync(admin, new [] {"Admin", "Moderator"}).Result;
                 }
             }
         }
