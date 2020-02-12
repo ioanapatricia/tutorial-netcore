896619b2deef167ff7910eec3bb8fae7600864cf
ioanapatricia <53076260+ioanapatricia@users.noreply.github.com>
2020-02-12 13:59:27 +0200

    17.179 Serving Static Files from the API
    -in the startup class:
    -added the support for serving static files.
    -line 135 --> looks for a default html or any of the default files that te web server would expect to find inside the wwwroot folder.
    -line 137 --> the server has the ability to use static files.
    -now when I run the api server I'm able to browse through the angular app on lh/5000.
    -refresh the page --> lh can't be found. Because the api server doesn't know about the routes in the angular app.
    -i'm using the angular routing to navigate between the pages.
    -as soon as I refresh the page, when I'm on an angular route, the api server doesn't know how to deal with that route. In only knows about the attribute based routing that I have inside the controllers.
    -to resolve that :
    -created a new controller(fallback.cs) which has the single responsability of serving the index.html file.
    -added another endpoint to fallback on the controller that I created. So it can serve that index.html and pass the routing responsability to angular.

--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -126,15 +126,20 @@ namespace DatingApp.API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
             app.UseAuthentication();
 
             app.UseAuthorization();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
